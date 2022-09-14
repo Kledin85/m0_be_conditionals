f0df69514ebe398cc0eb67e9047c6d1e6f85470a
Kyle Ledin <Kyle.Ledin85@gmail.com>
2022-09-14 12:01:38 -0500

    have the decision_making.rb file complete.

--- decision_making.rb
+++ decision_making.rb
@@ -5,7 +5,7 @@
 
 door_choice = 1
 bear_clothing = ""
-bear_choice = 1
+bear_choice = 2
 
 puts "You enter a dark room with two doors. Do you go through #1 or #2?"
 
@@ -35,9 +35,16 @@ end
 
 # Questions
 # 1. In English, using technical vocabulary, describe what is happening between lines 12 and 16.
+# => its asking what door the bear is going to choose and depending on what door is chosen it will change what clothing he has on
 # 2. What variable has a new value assigned to it after the first if statement executes?
+# => bear_clothing
 # 3. If you changed the variable door_choice to equal 3, what would the bear_clothing value be?
+# => the scarf
 # 4. In English, using technical vocabulary, describe what is happening between lines lines 25 and 33.
+# => using else if statements its deciding how the reader will interact with the bear depending on the bears choice.
 # 5. If you changed the variable bear_choice to equal 3, what will be the final outcome be?
+# => you will run as fast as you can into the next room. It's full of snakes!
 # 6. If you changed the variable door_choice to equal 1, and the variable bear_choice to equal 2, what will be the final outcome be?
+# => "you tell the bear its hat is to small and it starts to cry!"
 # 7. What is your favorite ending?
+# => the final else statement because have a bear as a friend sounds pretty cool.
