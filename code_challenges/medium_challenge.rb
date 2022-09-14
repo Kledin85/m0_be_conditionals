591475aac37c0cd1fac5cb168d0fdd8bde243d73
Kyle Ledin <Kyle.Ledin85@gmail.com>
2022-09-14 17:31:31 -0500

    Have the mild challenge done

---
+++ code_challenges/medium_challenge.rb
@@ -0,0 +1,9 @@
+fizzez = 45
+
+if (fizzez % 3 && 5 === 0)
+  puts "Fizz"
+elsif (fizzez % 5 === 0)
+  puts "Buzz"
+elsif (fizzez % 3 === 0)
+  puts "FizzBuzz"
+end
---
+++ code_challenges/mild_challenge.rb
@@ -0,0 +1,7 @@
+
+age = 21
+
+ if (age % 2 === 0)
+   puts "true"
+ elsif puts "false"
+ end
